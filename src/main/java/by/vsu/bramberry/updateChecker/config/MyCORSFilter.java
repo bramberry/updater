9806f32017de45483683b6c0309967354f70a969
bramberry <rshulga6@gmail.com>
2019-02-05 16:32:43 +0300

    add authorization header to cors

--- src/main/java/by/vsu/bramberry/updateChecker/config/MyCORSFilter.java
+++ src/main/java/by/vsu/bramberry/updateChecker/config/MyCORSFilter.java
@@ -1,12 +1,16 @@
 package by.vsu.bramberry.updateChecker.config;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//@Component
-//@Order(Ordered.HIGHEST_PRECEDENCE)
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class MyCORSFilter implements Filter {
 
 
@@ -20,7 +24,7 @@ public class MyCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me, Authorization");
         response.setHeader("Access-Control-Expose-Headers", "Authorization");
 
         chain.doFilter(req, res);
