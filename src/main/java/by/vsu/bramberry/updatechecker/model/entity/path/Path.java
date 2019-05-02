4e5de85ec9115c9e8afd0000c3779ac4fb2b6ae7
bramberry <rshulga6@gmail.com>
2019-05-02 16:06:24 +0300

    add Document annotation to path

--- src/main/java/by/vsu/bramberry/updatechecker/model/entity/path/Path.java
+++ src/main/java/by/vsu/bramberry/updatechecker/model/entity/path/Path.java
@@ -5,12 +5,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 
 
 @NoArgsConstructor
 @Data
+@Document
 public class Path implements Serializable {
 
     @Transient
